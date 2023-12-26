5e0efafe001182fca41b3f393590024e8a68bd37
Ertugrul Karademir <ekarademir@gmail.com>
2023-12-26 08:04:26 +0000

    refactor: separate out namespace module
    Signed-off-by: Ertugrul Karademir <ekarademir@gmail.com>

--- src/parser/namespace.rs
+++ src/parser/namespace.rs
@@ -1,6 +1,9 @@
 use nom::{
-    bytes::complete::tag, character::complete::space1, error::VerboseError,
-    sequence::separated_pair, IResult,
+    bytes::complete::tag,
+    character::complete::space1,
+    error::{context, ContextError, ParseError, VerboseError},
+    sequence::separated_pair,
+    IResult, Parser,
 };
 
 use super::common::token_parser;
@@ -10,26 +13,24 @@ use super::BuiltIn;
 #[derive(Debug, Eq, PartialEq, Clone)]
 pub struct NamespaceVersion((String, SemanticVersion));
 
-fn namespace_version_parser<'a>(
+fn namespace_version_parser<'a, E: ParseError<&'a str> + ContextError<&'a str>>(
     input: &'a str,
-) -> IResult<&'a str, NamespaceVersion, VerboseError<&'a str>> {
-    let mut parser = separated_pair(
-        token_parser,
-        tag("@"),
-        version_parser::<VerboseError<&'a str>>,
-    );
-    let (rest, (parsed_token, parsed_version)) = parser(input)?;
-    return Ok((
-        rest,
-        NamespaceVersion((parsed_token.to_string(), parsed_version)),
-    ));
+) -> IResult<&'a str, NamespaceVersion, E> {
+    context(
+        "NamespaceVersion",
+        separated_pair(token_parser, tag("@"), version_parser::<E>).map(
+            |(parsed_token, parsed_version)| {
+                NamespaceVersion((parsed_token.to_string(), parsed_version))
+            },
+        ),
+    )(input)
 }
 
 pub fn namespace_parser<'a>(input: &'a str) -> IResult<&'a str, BuiltIn, VerboseError<&'a str>> {
     let mut parser = separated_pair(
         tag("namespace"),
         space1::<_, VerboseError<&'a str>>,
-        namespace_version_parser,
+        namespace_version_parser::<VerboseError<&'a str>>,
     );
 
     let (rest, (_namespace_tag, parsed_fqn)) = parser(input)?;
@@ -40,11 +41,12 @@ pub fn namespace_parser<'a>(input: &'a str) -> IResult<&'a str, BuiltIn, Verbose
 #[cfg(test)]
 mod test {
     use super::{BuiltIn, NamespaceVersion, SemanticVersion};
+    use nom::error::VerboseError;
 
     #[test]
     fn test_namespace_version() {
         assert_eq!(
-            super::namespace_version_parser("test@12.13.14"),
+            super::namespace_version_parser::<VerboseError<&str>>("test@12.13.14"),
             Ok((
                 "",
                 NamespaceVersion((
@@ -54,7 +56,7 @@ mod test {
             )),
         );
         assert_eq!(
-            super::namespace_version_parser("test@12.13.14-pre"),
+            super::namespace_version_parser::<VerboseError<&str>>("test@12.13.14-pre"),
             Ok((
                 "",
                 NamespaceVersion((
